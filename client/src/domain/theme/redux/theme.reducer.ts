bd57e60c3b3cab86749af9ba95f95111bb9ae4f7
hrishix6 <hrishi.pethe@gmail.com>
2023-11-25 08:58:22 +0530

    add dark mode theme support

---
+++ client/src/domain/theme/redux/theme.reducer.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from "@/lib/store";
+import { THEME_LOCAL_STORAGE_KEY } from "@/lib/constants";
+
+export type Theme = 'dark' | 'light' | 'system';
+
+export interface ThemeState {
+    theme: Theme
+}
+
+const initialState: ThemeState = {
+    theme: localStorage.getItem(THEME_LOCAL_STORAGE_KEY) as Theme || 'system'
+}
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        setTheme: (state, action: PayloadAction<Theme>) => {
+            localStorage.setItem(THEME_LOCAL_STORAGE_KEY, action.payload);
+            state.theme = action.payload
+        }
+    }
+});
+
+export const { setTheme } = themeSlice.actions;
+
+export const themeReducer = themeSlice.reducer;
+
+export const selectTheme = (state: RootState) => state.themeStore.theme;
