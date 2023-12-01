da3a856ba6c9e57695927e534f138ed5852d1f1e
hrishix6 <hrishi.pethe@gmail.com>
2023-12-01 08:47:35 +0530

    declare global types for 3rd party scripts

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,27 @@
 /// <reference types="vite/client" />
+
+declare interface ZipFileInput {
+    name: string;
+    stream: () => ReadableStream<Uint8Array> | null
+    directory?: boolean;
+}
+
+declare interface ZipControl {
+    enqueue: (input: ZipFileInput) => void;
+    close: () => void;
+}
+
+declare interface ZipOptions {
+    start: (ctrl: ZipControl) => void;
+    pull: (ctrl: ZipControl) => Promise<void>
+}
+
+interface Window {
+    ZIP: {
+        new(arg: ZipOptions): ReadableStream
+    }
+}
+
+declare const streamSaver = {
+    createWriteStream: (...args: any[]) => any
+}
